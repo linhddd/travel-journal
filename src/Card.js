4f1dcf8216e98341dbd05d16602c7621b1d44b1e
linh <ldie@hellofresh.com>
2022-08-14 12:46:34 +0200

    style the page

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,11 @@ export default function Card(props) {
     return (
         <div className="card">
             <img className="card-img"src={props.item.imageUrl}/>
-            <div className="content">
+            <div className="card-content">
                 <div className="card--location">
                     <img src="./location-icon.svg" />
                     <span>{props.item.location}</span>
-                    <span><a href={props.item.googleMapsUrl}>View on Google Maps</a></span>
+                    <span><a href={props.item.googleMapsUrl} target="blank">View on Google Maps</a></span>
                 </div>
                 <h2 className="card--title">{props.item.title}</h2>
                 <p className="card--time">{props.item.startDate}{props.item.endDate}</p>
