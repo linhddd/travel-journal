1cebb220097651ebe86eb6da160d807fa461be47
linh <ldie@hellofresh.com>
2022-09-09 11:12:06 +0200

    destructure

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,34 @@
-export default function Card(props) {
-    return (
-        <div className="card">
-            <img className="card-img"src={props.item.imageUrl}/>
-            <div className="card-content">
-                <div className="card--location">
-                    <img src="./location-icon.svg" />
-                    <span>{props.item.location}</span>
-                    <span><a href={props.item.googleMapsUrl} target="blank">View on Google Maps</a></span>
-                </div>
-                <h2 className="card--title">{props.item.title}</h2>
-                <p className="card--time">{props.item.startDate}{props.item.endDate}</p>
-                <p className="card--description">{props.item.description}</p>
-            </div>
+export default function Card({
+  item: {
+    imageUrl,
+    location,
+    googleMapsUrl,
+    title,
+    startDate,
+    endDate,
+    description,
+  },
+}) {
+  return (
+    <div className="card">
+      <img className="card-img" src={imageUrl} />
+      <div className="card-content">
+        <div className="card--location">
+          <img src="./location-icon.svg" />
+          <span>{location}</span>
+          <span>
+            <a href={googleMapsUrl} target="blank">
+              View on Google Maps
+            </a>
+          </span>
         </div>
-    )
+        <h2 className="card--title">{title}</h2>
+        <p className="card--time">
+          {startDate}
+          {endDate}
+        </p>
+        <p className="card--description">{description}</p>
+      </div>
+    </div>
+  );
 }
